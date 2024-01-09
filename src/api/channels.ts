5cdfb594429e9734b1944b13b01e319034678748
skirtle <65301168+skirtles-code@users.noreply.github.com>
2024-01-09 11:25:07 +0000

    Fix TS problems after upgrading

--- src/api/channels.ts
+++ src/api/channels.ts
@@ -77,7 +77,7 @@ export async function loadThreadableChannels(bot: Bot) {
 
   for (const channel of viewableChannels) {
     if (
-      (channel.isTextBased() || channel.type === ChannelType.GuildForum) &&
+      (channel.isTextBased() || channel.isThreadOnly()) &&
       !channel.isVoiceBased() &&
       channel.threads
     ) {
